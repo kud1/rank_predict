b9f6e9cc9880ede39790ade67d513523551a351c
zhangle <iamkudi@163.com>
2017-10-30 15:45:36 +0800

    update config file

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 DATASET_DB = {
-    "host": "****",
-    "password": "****",
-    "db": "***",
-    "user": "***",
+    "host": "123.206.214.163",
+    "password": "ZhangLe@0910",
+    "db": "rank_yuce",
+    "user": "zhangle",
+    "charset": "utf8",
 }
 
 # 为每个feature设置weight
@@ -14,9 +15,8 @@ WEIGHT = {
         'ques_focus_cnt': 1,
         'ques_view_cnt': 1,
         'ques_ans_cnt': 1,
-        'content': 1,
-        'title': 1,
-        'keyword': 1,
+        'title_tf_idf': 2,
+        'content_tf_idf': 1,
         'author_ans_cnt': 1,
         'author_follower_cnt': 1,
         'author_article_cnt': 1,
